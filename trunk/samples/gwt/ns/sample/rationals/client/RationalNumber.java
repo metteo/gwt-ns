57cbf0c1aeea311237a8476b15bc6384bdf5545e
bckenny <bckenny@ee7e7a16-d65f-11de-b13e-7d68ceb8f08a>
2010-01-31 13:17:30 +0000

    First Web Worker sample project. Demonstrates enumeration of rational numbers.

---
+++ trunk/samples/gwt/ns/sample/rationals/client/RationalNumber.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2010 Brendan Kenny
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package gwt.ns.sample.rationals.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+/**
+ * A javascript object representing a rational number.
+ */
+public class RationalNumber extends JavaScriptObject {
+	protected RationalNumber() {
+		// required hidden JavaScriptObject constructor
+	}
+	
+	/**
+	 * @param numerator
+	 * @param denominator
+	 * @return A new rational number javascript object.
+	 */
+	public static final native RationalNumber create(int numerator, int denominator) /*-{
+		return { "n" : numerator, "d" : denominator };
+  	}-*/;
+	
+	/**
+	 * @return The denominator of this rational
+	 */
+	public final native int getDenominator() /*-{
+		return this.d;
+	}-*/;
+	
+	/**
+	 * @return The numerator of this rational
+	 */
+	public final native int getNumerator() /*-{
+		return this.n;
+  	}-*/;
+}
